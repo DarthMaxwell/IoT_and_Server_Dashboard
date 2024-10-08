68f15622447308ddc2238318d008acbfa10c11f6
Maxwell <maxwell.wogsland@gmail.com>
2024-10-08 19:25:49 +0200

    Initial commit

---
+++ spring_boot/dashboard/src/main/java/com/personal/dashboard/IoTAndServerDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.personal.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTAndServerDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoTAndServerDashboardApplication.class, args);
+	}
+
+}
