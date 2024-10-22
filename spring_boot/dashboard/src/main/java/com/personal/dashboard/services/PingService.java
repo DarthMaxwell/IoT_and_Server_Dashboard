5c2c16533e62853eddf319880a71756554b64d68
Maxwell <maxwell.wogsland@gmail.com>
2024-10-22 17:36:10 +0200

    added ping system to start showing if hosts alive

---
+++ spring_boot/dashboard/src/main/java/com/personal/dashboard/services/PingService.java
@@ -0,0 +1,21 @@
+package com.personal.dashboard.services;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PingService {
+	// Change these
+	private final byte[] PiHole = new byte[] { (byte) 192, (byte) 168, 86, (byte) 163};
+	private final byte[] FileSharing = new byte[] {(byte) 192, (byte) 168, 86, (byte) 164}; 
+
+	public boolean[] ping() throws UnknownHostException, IOException {
+		InetAddress inet1 = InetAddress.getByAddress(PiHole);
+		InetAddress inet2 = InetAddress.getByAddress(FileSharing);
+		
+		return new boolean[] {inet1.isReachable(1000), inet2.isReachable(1000)};
+	}
+}
